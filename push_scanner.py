4cbabc9a4b16081eb609221065f83424b25d8366
Kay <ibotforwin@gmail.com>
2020-02-16 20:03:44 -0500

    Implemented a door frame and an object recognition system.

--- push_scanner.py
+++ push_scanner.py
@@ -1,28 +1,43 @@
 from math import pi, sin, cos
 import matplotlib.pyplot as plt
 from rplidar import RPLidar
-
+lidar = RPLidar('COM7', baudrate=115200)
+plt.ion()
+plt.show()
+range_mm=2500
 def get_data():
     x=[]
     y=[] 
-    lidar = RPLidar('COM7', baudrate=115200)
-    lidar.set_pwm(pwm=800)
+    
+
     counter=0
     for scan in lidar.iter_scans(max_buf_meas=500):
+        file1 = open("MyFile.txt","a")
+        file1.write(str(scan))
+        file1.close()
+        plot_data(x,y)
         x=[]
         y=[]
+            
         for point in scan:
-            x.append(point[2] * sin(point[1] * pi / 180))
-            y.append(point[2] * cos(point[1] * pi / 180))
-        plt.clf()
-        plt.scatter(x, y, marker='o', s=1)
-        plt.xlim(-1000,1000)
-        plt.ylim(-1000,1000)
-        plt.pause(.0001)
- 
 
-    lidar.stop()
+            x.append(point[2] * -1*sin(point[1] * pi / 180))
+            y.append(point[2] * -1*cos(point[1] * pi / 180))
+            print(str(point[2] * -1*sin(point[1] * pi / 180))+" "+str(point[2] * -1*cos(point[1] * pi / 180)))
+        
+
+ 
+    
+   
     return scan
-plt.show()
+
+def plot_data(x, y):
+
+    plt.clf()
+    plt.xlim(-range_mm, range_mm)
+    plt.ylim(-range_mm, 0)
+    plt.scatter(x, y, marker='o', s=.5)
+    plt.pause(0.01)
+
 
 get_data()
